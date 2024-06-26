db6ad7f8f09799907eb72e337550e33313daf2a4
Dtan713 <s.donaldtan@gmail.com>
2024-06-26 16:09:06 -0400

    adding first part

---
+++ src/CoreJavaBasics.java
@@ -0,0 +1,11 @@
+public class CoreJavaBasics {
+    public static void main(String[] args){
+        int y = 5;
+        int z = 5;
+        int x = y + z;
+        System.out.println("+ operator resulted in " + x);
+        x = y + z;
+
+
+    }
+}
