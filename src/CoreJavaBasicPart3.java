807fb252afbf16f7ea9213addc42ae6dca375e93
Dtan713 <s.donaldtan@gmail.com>
2024-06-26 18:39:29 -0400

    adding more of part 3

--- src/CoreJavaBasicPart3.java
+++ src/CoreJavaBasicPart3.java
@@ -1,9 +1,12 @@
-public class CoreJavaBasicPart3 {
+public class CoreJavaBasic Part3 {
     public static void main(String[] args) {
         int num1 = 5;
         double num2 = 15;
 
-        double sum
+        double sum = num1 + num2;
+
+        System.out.println( + num1 + num2); .
+
 
 
     }
