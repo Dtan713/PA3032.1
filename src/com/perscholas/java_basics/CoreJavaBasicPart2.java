afc2c6fecb362fd4af5ca0851a9e9ba8217434c5
Dtan713 <s.donaldtan@gmail.com>
2024-06-26 18:27:53 -0400

    done

---
+++ src/com/perscholas/java_basics/CoreJavaBasicPart2.java
@@ -0,0 +1,9 @@
+package com.perscholas.java_basics;
+
+public class CoreJavaBasicPart2 {
+    double a1 = 2;
+    double a2 = 2;
+
+    double sum = a1 + a2;
+    System.out.println("The sum of " + a1 = 2 + a2 = 2);
+
--- src/com/perscholas/java_basics/CoreJavaBasicsPar1.java
+++ src/com/perscholas/java_basics/CoreJavaBasicsPar1.java
@@ -1,4 +1,6 @@
-public class CoreJavaBasics {
+package com.perscholas.java_basics;
+
+public class CoreJavaBasicsPar1 {
     public static void main(String[] args){
         int y = 5;
         int z = 5;
