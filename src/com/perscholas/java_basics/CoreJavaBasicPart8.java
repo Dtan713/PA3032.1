f539da365187962cedea6d199f8ba4bc6383cdda
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 10:35:59 -0400

    some of part 8

---
+++ src/com/perscholas/java_basics/CoreJavaBasicPart8.java
@@ -0,0 +1,12 @@
+package com.perscholas.java_basics;
+
+public class CoreJavaBasicPart8 {
+    public static void main(String[] args) {
+        int a = coffee;
+        int b = espresso;
+        int c = cappuccino;
+        int d = subtotal;
+        int e = totalSale;
+
+    }
+}
