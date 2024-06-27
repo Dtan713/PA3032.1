f30ee24a6c9f68ad3c372e9fedb15ecafd37c6ac
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 10:17:50 -0400

    part5

---
+++ src/com/perscholas/java_basics/CoreJavaBasicPart5.java
@@ -0,0 +1,16 @@
+package com.perscholas.java_basics;
+
+public class CoreJavaBasicPart5 {
+    public static void main(String[] args) {
+
+        double x = 5.5;
+        double y = 1.5;
+
+        double divided = (x/y);
+
+        System.out.println(divided);
+
+
+    }
+
+    }
