92f2b4df8e17f6a0a70949e673d8e6dcbcb339a0
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 10:31:47 -0400

    part7

---
+++ src/com/perscholas/java_basics/CoreJavaBasicPart7.java
@@ -0,0 +1,16 @@
+package com.perscholas.java_basics;
+
+
+public class CoreJavaBasicPart7 {
+     static final int today = 5;
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x * today;
+
+
+        System.out.println(y);
+
+
+
+    }
+}
