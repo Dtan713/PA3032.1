ff8bf236e326bde2ad10c2980877442e7c55ea56
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 09:00:51 -0400

    some of part 4

---
+++ src/CoreJavaBasicPart4.java
@@ -0,0 +1,11 @@
+public class CoreJavaBasicPart4 {
+    public static void main(String[] args) {
+
+        double num1 = 8;
+        double num2 = 2;
+
+        double div = "8 / 2";
+        System.out.println(num1 / num2);
+
+    }
+}
