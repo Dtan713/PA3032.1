ec312201d15638007121388d280d62358b8880be
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 10:58:24 -0400

    part8

--- src/com/perscholas/java_basics/CoreJavaBasicPart8.java
+++ src/com/perscholas/java_basics/CoreJavaBasicPart8.java
@@ -1,12 +1,23 @@
 package com.perscholas.java_basics;
 
 public class CoreJavaBasicPart8 {
+    final static double SALES_TAX = 0.08;
     public static void main(String[] args) {
-        int a = coffee;
-        int b = espresso;
-        int c = cappuccino;
-        int d = subtotal;
-        int e = totalSale;
+
+        int coffee = 3;
+        int espresso = 4;
+        int cappuccino = 5;
+        double subtotal;
+        double totalSale;
+
+        subtotal = (coffee + espresso + cappuccino) * SALES_TAX;
+        totalSale = subtotal;
+
+        System.out.println(totalSale);
+
+
+
+
 
     }
 }
