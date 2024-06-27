fae2df6ca0b2681311d8ed39f5b5d92c7d1d54bb
Dtan713 <s.donaldtan@gmail.com>
2024-06-27 10:21:38 -0400

    part6

---
+++ src/com/perscholas/java_basics/CoreJavaBasicPart6.java
@@ -0,0 +1,14 @@
+package com.perscholas.java_basics;
+
+public class CoreJavaBasicPart6 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+
+
+        double q = (x/y);
+
+        System.out.println(q);
+
+    }
+}
